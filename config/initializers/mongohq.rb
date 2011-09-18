7751c7e13f34dc7eccbcca7dabcafa054811a435
liors <lior@billguard.com>
2011-09-18 16:05:01 +0300

    1

--- config/initializers/mongohq.rb
+++
@@ -1,7 +0,0 @@
-MONGOHQ::Base.establish_connection!(
-  :host     => ENV['MONGOID_HOST'],
-  :port => ENV['MONGOID_PORT'],
-  :username     => ENV['MONGOID_USERNAME'],
-  :password     => ENV['MONGOID_PASSWORD'],
-  :database     => ENV['MONGOID_DATABASE']
-)
