cbfc19845562dbd3540599d12a2b1f2f2eb78a8d
liors <lior@billguard.com>
2011-09-19 09:28:32 +0300

    clean up

--- main.rb
+++ main.rb
@@ -3,11 +3,13 @@ require 'sinatra'
 require 'haml'
 require 'mongoid'
 require 'json'
+require 'model/email'
 
 configure do
    Mongoid.configure do |config|
     name = 'app998473'
     config.master = Mongo::Connection.from_uri(ENV['MONGOHQ_URL']).db(name)
+    #config.master = Mongo::Connection.new.db(name)
    end
 end
 
@@ -17,15 +19,7 @@ end
 
 
 get '/api/email' do content_type :json
-  email = Email.new(:email => 'foo')
-  email.save
-
   email = Email.all_in()
   email.to_json
   
-end
-
-class Email
-  include Mongoid::Document
-  field :email
 end
