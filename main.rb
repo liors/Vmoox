f7f604a9290b2babcadb29b91b585a1647d73b02
lior <lior@billguard.com>
2012-04-25 08:24:46 +0300

    vmoox - new effectiveIT work

--- main.rb
+++ main.rb
@@ -8,13 +8,17 @@ require 'model/email'
 configure do
    Mongoid.configure do |config|
     name = 'app998473'
-    config.master = Mongo::Connection.from_uri(ENV['MONGOHQ_URL']).db(name)
-    #config.master = Mongo::Connection.new.db(name)
+    #config.master = Mongo::Connection.from_uri(ENV['MONGOHQ_URL']).db(name)
+    config.master = Mongo::Connection.new.db(name)
    end
 end
 
 get '/' do 
   haml :index
+end
+
+get '/effectiveIT' do 
+  erb :effectiveIT
 end
 
 
